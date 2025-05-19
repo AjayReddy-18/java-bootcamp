b12a54ebc73d16b3a56c826ce992566bac35fd7d
Ajay Reddy <iamajayreddy18@gmail.com>
2025-05-19 12:43:31 +0530

    adds probabilityOfNot method in FlippingProbability Class

--- src/models/FlippingProbability.java
+++ src/models/FlippingProbability.java
@@ -1,14 +1,24 @@
 package models;
 
+import java.util.Arrays;
+
 public class FlippingProbability {
 
-    private final String[] events;
+    private final String[] sampleSpace;
 
-    public FlippingProbability(String[] events) {
-        this.events = events;
+    public FlippingProbability(String[] sampleSpace) {
+        this.sampleSpace = sampleSpace;
     }
 
-    public double probablityOf(String tail) {
-        return 0.5;
+    public double probabilityOf(String event) {
+        double totalOutcomes = this.sampleSpace.length;
+        long favourableOutcomes = Arrays.stream(this.sampleSpace).filter(x -> x.equals(event)).count();
+        return (double) favourableOutcomes / totalOutcomes;
+    }
+
+    public double probabilityOfNot(String event) {
+        double totalOutcomes = this.sampleSpace.length;
+        long favourableOutcomes = Arrays.stream(this.sampleSpace).filter(x -> !x.equals(event)).count();
+        return (double) favourableOutcomes / totalOutcomes;
     }
 }
--- test/models/FlippingProbabilityTest.java
+++ test/models/FlippingProbabilityTest.java
@@ -8,7 +8,21 @@ public class FlippingProbabilityTest {
     @Test
     void probabilityOfTails() {
         FlippingProbability flippingProbability = new FlippingProbability(new String[]{"HEAD", "TAIL"});
-        double probablityOfTails = flippingProbability.probablityOf("TAIL");
-        assertEquals(0.5, probablityOfTails);
+        double probabilityOfTails = flippingProbability.probabilityOf("TAIL");
+        assertEquals(0.5, probabilityOfTails);
+    }
+
+    @Test
+    void probabilityOfHeads() {
+        FlippingProbability flippingProbability = new FlippingProbability(new String[]{"HEAD", "TAIL"});
+        double probabilityOfHeads = flippingProbability.probabilityOf("HEAD");
+        assertEquals(0.5, probabilityOfHeads);
+    }
+
+    @Test
+    void probabilityOfNotTails() {
+        FlippingProbability flippingProbability = new FlippingProbability(new String[]{"HEAD", "TAIL"});
+        double probabilityOfNotTails = flippingProbability.probabilityOfNot("TAIL");
+        assertEquals(0.5, probabilityOfNotTails);
     }
 }
